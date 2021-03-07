f08c688f12422fe598313969bf83b0b0fb90676b
Jeremy Philipson <jeremyphilipson@gmail.com>
2021-03-06 19:06:29 -0500

    chore: center header on mobile nav

--- components/shared/Navigation.js
+++ components/shared/Navigation.js
@@ -43,8 +43,8 @@ const Navigation = ({ title }) => {
 
   return (
     <header className='sticky top-0 z-50 text-gray-700 bg-blue-100 border-blue-300 sm:border-t sm:border-b body-font'>
-      <nav className='flex-col items-center justify-between px-2 py-2 border-b border-blue-300 max-w-screen-xl sm:px-0 sm:mx-8 sm:flex sm:items-center sm:flex-row md:mx-12 lg:mx-16 2xl:mx-auto sm:border-0'>
-        <div className='flex items-center py-1 text-center sm:hidden'>
+      <nav className='flex-col items-center justify-between max-w-screen-xl px-2 py-2 border-b border-blue-300 sm:px-0 sm:mx-8 sm:flex sm:items-center sm:flex-row md:mx-12 lg:mx-16 2xl:mx-auto sm:border-0'>
+        <div className='grid py-1 sm:hidden' style={{ gridTemplateColumns: '50px 1fr 50px'}}>
           <button
             onClick={() => setIsOpen(!isOpen)}
             type='button'
@@ -52,7 +52,7 @@ const Navigation = ({ title }) => {
           >
             {isOpen ? <NakedXIcon /> : <HamburgerIcon />}
           </button>
-          <h3 className="ml-4 text-xl text-gray-700">{title}</h3>
+          <h3 className="my-auto text-xl text-center text-gray-700">{title}</h3>
         </div>
         <div
           className={`sm:mt-0 sm:flex sm:visible sm:opacity-100 sm:h-auto items-center justify-center text-base transition-opacity ${
