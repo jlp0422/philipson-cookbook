edfb43210316af5e94eb322d84469289ffe617c7
Jeremy Philipson <jeremyphilipson@gmail.com>
2021-12-19 17:49:32 -0500

    move pill into own component

--- components/shared/filter/SelectedFilters.js
+++ components/shared/filter/SelectedFilters.js
@@ -1,20 +1,5 @@
-import CircledXIcon from '~/icons/CircledX'
+import Pill from '~/components/shared/Pill'
 
-const Pill = ({ children, onClear }) => {
-  return (
-    <div className='flex items-center px-4 py-2 text-sm border text-slate-600 rounded-3xl border-slate-400 bg-slate-200 whitespace-nowrap'>
-      <span>{children}</span>
-      {onClear ? (
-        <button
-          className='w-5 h-5 ml-2 font-semibold text-gray-600 disabled:text-gray-400'
-          onClick={onClear}
-        >
-          <CircledXIcon />
-        </button>
-      ) : null}
-    </div>
-  )
-}
 
 const SelectedFilters = ({
   // selectedTags,
